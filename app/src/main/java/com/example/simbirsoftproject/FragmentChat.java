b032ecea295878d6f377ddd865d094c38c085019
Anton Loginov <antonlogin32@gmail.com>
2020-10-11 21:22:05 +0300

    Добавлена рилтайм бд, изменён хмл файл фрагмент_чат

--- app/src/main/java/com/example/simbirsoftproject/FragmentChat.java
+++ app/src/main/java/com/example/simbirsoftproject/FragmentChat.java
@@ -1,15 +1,28 @@
 package com.example.simbirsoftproject;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class FragmentChat extends Fragment {
+
+    public static final String TAG = "messages";
+    EditText edMessage;
+    Button btnSendMessage;
 
     @Nullable
     @Override
@@ -17,5 +30,12 @@ public class FragmentChat extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_chat, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("messages");
     }
 }
